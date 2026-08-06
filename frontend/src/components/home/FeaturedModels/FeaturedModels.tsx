dc6d2630cdcf1e6eb06a4d06b6a0361fd81ca3b6
ultramegared <301172501+ultramegared@users.noreply.github.com>
2026-08-06 14:18:27 -0500

    feat(home): create featured models component

---
+++ frontend/src/components/home/FeaturedModels/FeaturedModels.tsx
@@ -0,0 +1,33 @@
+/**
+ * ===============================================================
+ * Author: ultramegared
+ * Project: Magic Touch Designs
+ * File: FeaturedModels.tsx
+ * Module: Home
+ * Language: TypeScript React
+ * Description:
+ * Featured models section.
+ * ===============================================================
+ */
+
+import "./FeaturedModels.css";
+
+function FeaturedModels() {
+
+    return (
+
+        <section>
+
+            <h2>
+
+                Featured Models
+
+            </h2>
+
+        </section>
+
+    );
+
+}
+
+export default FeaturedModels;
---
+++ frontend/src/components/home/FeaturedModels/FeaturedModels.types.ts
@@ -0,0 +1,23 @@
+/**
+ * ===============================================================
+ * Author: ultramegared
+ * Project: Magic Touch Designs
+ * File: FeaturedModels.types.ts
+ * Module: Home
+ * Language: TypeScript
+ * Description:
+ * Featured Models component types.
+ * ===============================================================
+ */
+
+export interface FeaturedModel {
+
+    id: number;
+
+    name: string;
+
+    image: string;
+
+    price: number;
+
+}
