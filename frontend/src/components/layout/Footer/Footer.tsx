dedd5daee46e5c25b0b081353c02db046bbaa530
ultramegared <301172501+ultramegared@users.noreply.github.com>
2026-08-06 09:35:49 -0500

    feat(footer): create main footer component

---
+++ frontend/src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+/**
+ * ===============================================================
+ * Author: ultramegared
+ * Project: Magic Touch Designs
+ * File: Footer.tsx
+ * Module: Frontend
+ * Language: TypeScript React
+ * Description:
+ * Main website footer.
+ * ===============================================================
+ */
+
+import "./Footer.css";
+
+function Footer() {
+
+    return (
+
+        <footer>
+
+            Footer
+
+        </footer>
+
+    );
+
+}
+
+export default Footer;
---
+++ frontend/src/components/layout/Footer/Footer.types.ts
@@ -0,0 +1,21 @@
+/**
+ * ===============================================================
+ * Author: ultramegared
+ * Project: Magic Touch Designs
+ * File: Footer.types.ts
+ * Module: Frontend
+ * Language: TypeScript
+ * Description:
+ * Footer types.
+ * ===============================================================
+ */
+
+export interface FooterLink {
+
+    id: number;
+
+    label: string;
+
+    path: string;
+
+}
