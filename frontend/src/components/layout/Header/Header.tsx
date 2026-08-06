c1624155de6097f75b8bd4febd23e83e194270ae
ultramegared <301172501+ultramegared@users.noreply.github.com>
2026-08-06 02:11:26 -0500

    feat(header): create main header component

---
+++ frontend/src/components/layout/Header/Header.tsx
@@ -0,0 +1,27 @@
+/**
+ * ===============================================================
+ * Author: ultramegared
+ * Project: Magic Touch Designs
+ * File: Header.tsx
+ * Module: Frontend
+ * Language: TypeScript React
+ * Description:
+ * Main website header.
+ * ===============================================================
+ */
+
+function Header() {
+
+    return (
+
+        <header>
+
+            Header
+
+        </header>
+
+    );
+
+}
+
+export default Header;
