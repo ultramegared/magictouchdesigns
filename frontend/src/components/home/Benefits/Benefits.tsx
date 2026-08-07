f43f5976121599895d2ecdefe7fbe2a06afbc148
ultramegared <301172501+ultramegared@users.noreply.github.com>
2026-08-07 11:09:14 -0500

    git add . git commit -m "fix(benefits): restore component after render test" git push origin main

--- frontend/src/components/home/Benefits/Benefits.tsx
+++ frontend/src/components/home/Benefits/Benefits.tsx
@@ -1,17 +1,87 @@
-return (
+/**
+ * ================================================================
+ * Author: ultramegared
+ * Project: Magic Touch Designs
+ * File: Benefits.tsx
+ * Module: Home
+ * Language: TypeScript React
+ * Description:
+ * Benefits Section.
+ * ================================================================
+ */
 
-    <section
-        style={{
-            background: "red",
-            color: "white",
-            padding: "50px",
-            fontSize: "40px",
-            textAlign: "center"
-        }}
-    >
+import "./Benefits.css";
 
-        BENEFITS TEST
+import { benefits } from "./Benefits.data";
 
-    </section>
+function Benefits() {
 
-);
+    return (
+
+        <section className="benefits">
+
+            <div className="benefits__container">
+
+                {
+
+                    benefits.map(
+
+                        (
+
+                            benefit
+
+                        ) => {
+
+                            const Icon = benefit.icon;
+
+                            return (
+
+                                <article
+
+                                    key={benefit.id}
+
+                                    className="benefit"
+
+                                >
+
+                                    <div className="benefit__icon">
+
+                                        <Icon size={38} />
+
+                                    </div>
+
+                                    <div className="benefit__content">
+
+                                        <h3>
+
+                                            {benefit.title}
+
+                                        </h3>
+
+                                        <p>
+
+                                            {benefit.description}
+
+                                        </p>
+
+                                    </div>
+
+                                </article>
+
+                            );
+
+                        }
+
+                    )
+
+                }
+
+            </div>
+
+        </section>
+
+    );
+
+}
+
+export default Benefits;
