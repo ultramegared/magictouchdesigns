0c2afbce895ae35f2fb55fbe39c79e2cbf8ea741
ultramegared <301172501+ultramegared@users.noreply.github.com>
2026-08-12 15:08:31 -0500

    fix: restore how it works component

--- frontend/src/components/home/HowItWorks/HowItWorks.tsx
+++ frontend/src/components/home/HowItWorks/HowItWorks.tsx
@@ -14,66 +14,49 @@ import "./HowItWorks.css";
 import { howItWorksSteps } from "./HowItWorks.data";
 
 function HowItWorks() {
-
     return (
-
         <section className="how-it-works">
-
             <div className="how-it-works__container">
 
                 <div className="how-it-works__header">
-
                     <span className="how-it-works__eyebrow">
                         HOW IT WORKS
                     </span>
-
                 </div>
 
                 <div className="how-it-works__steps">
 
                     {howItWorksSteps.map((step, index) => (
-
                         <div
                             className="how-it-works__item"
                             key={step.id}
                         >
-
                             <div className="how-it-works__step">
 
                                 <div className="how-it-works__number">
                                     {step.id}
                                 </div>
 
-                                <h3>
-                                    {step.title}
-                                </h3>
+                                <h3>{step.title}</h3>
 
-                                <p>
-                                    {step.description}
-                                </p>
+                                <p>{step.description}</p>
 
                             </div>
 
                             {index < howItWorksSteps.length - 1 && (
-
                                 <span className="how-it-works__arrow">
                                     →
                                 </span>
-
                             )}
 
                         </div>
-
                     ))}
 
                 </div>
 
             </div>
-
         </section>
-
     );
-
 }
 
 export default HowItWorks;
